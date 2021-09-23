a7c7f0d528db3202a39414cf7779fb01fdec0804
Attila Czibere <czibere.a@gmail.com>
2021-09-23 12:20:56 +0200

    Implementing the INI reading feature

--- GettingStartedOnGitHub/Form1.Designer.cs
+++ GettingStartedOnGitHub/Form1.Designer.cs
@@ -33,12 +33,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(363, 179);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 38);
             this.button1.TabIndex = 0;
             this.button1.Text = "Gomb";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
--- GettingStartedOnGitHub/Form1.cs
+++ GettingStartedOnGitHub/Form1.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using IniParser;
+using IniParser.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +16,23 @@ namespace GettingStartedOnGitHub
 {
     public partial class Form1 : Form
     {
+        int upper_sentence_to_be_displayed = 1;
+        int lower_sentence_to_be_displayed = 1;
+
         public Form1()
         {
             InitializeComponent();
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile(Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Config","config.ini"));
+            string useFullScreenStr = data["last_displayed_sentence_numbers"]["upper"];
+            upper_sentence_to_be_displayed = int.Parse(useFullScreenStr);
+            useFullScreenStr = data["last_displayed_sentence_numbers"]["lower"];
+            lower_sentence_to_be_displayed = int.Parse(useFullScreenStr);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(upper_sentence_to_be_displayed.ToString());
         }
     }
 }
